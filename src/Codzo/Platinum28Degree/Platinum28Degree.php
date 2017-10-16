ebcd20cd8dfa7460065cc2f4f353ef0b1d5c2d8c
neilfan <neil.fan@codzo.com>
2017-10-16 16:09:38 +1100

    code reflection

--- src/Codzo/Platinum28Degree/Platinum28Degree.php
+++ src/Codzo/Platinum28Degree/Platinum28Degree.php
@@ -33,34 +33,30 @@ class Platinum28Degree
     const URL_FORM  = 'https://28degrees-online.latitudefinancial.com.au/fcc/ealogin.fcc';
 
     /**
-     * The "Home" page after login, which includes account info and latest 20
-     * transactions.
+     * The Config object
      */
-    protected $cached_html;
-
-    /**
-     * The path of cache file
-     */
-    protected $cache_filepath;
+    protected $config;
 
     /**
      * constructor
      */
     public function __construct()
     {
-        $this->cache_filepath = sys_get_temp_dir() . '/codzo.p28d.cache';
-
-        if (is_readable($this->cache_filepath)) {
-            $this->cached_html = file_get_contents($this->cache_filepath);
-        }
+        $this->config = new Config(__DIR__ . '/../../../config/');
     }
 
     /**
      * Login to 28degree website, retrieve "Home" page and update the cache.
+     * @param bool $force_reload force to reload the content
      * @return string the html
      */
-    public function updateCache()
+    public function loadContent($force_reload = false)
     {
+        // if has cache and it's valid
+        if (!$force_reload && $this->isCacheValid()) {
+            return file_get_contents($this->getCacheFilePath());
+        }
+
         $cookie_file = tempnam(sys_get_temp_dir(), 'codzo.p28d.session.');
 
         // create a new cURL resource
@@ -74,8 +70,6 @@ class Platinum28Degree
         curl_setopt($ch, CURLOPT_COOKIESESSION, true);
         curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_file);
         curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file);
-
-        $config = new Config(__DIR__ . '/../../../config');
 
         // get and set cookies
         curl_setopt($ch, CURLOPT_URL, self::URL_ACCESS);
@@ -91,8 +85,8 @@ class Platinum28Degree
             $form_data = array_combine($matches[1], $matches[2]);
 
             // plus the username and password
-            $form_data['USER']     = $config->get('app.username');
-            $form_data['PASSWORD'] = $config->get('app.password');
+            $form_data['USER']     = $this->config->get('app.username');
+            $form_data['PASSWORD'] = $this->config->get('app.password');
 
             curl_setopt($ch, CURLOPT_URL, self::URL_FORM);
             curl_setopt($ch, CURLOPT_POST, true);
@@ -103,7 +97,9 @@ class Platinum28Degree
 
             if ($html) {
                 $this->cached_html = $html;
-                file_put_contents($this->cache_filepath, $html, LOCK_EX);
+                if ($cache_filepath = $this->getCacheFilePath()) {
+                    file_put_contents($cache_filepath, $html, LOCK_EX);
+                }
             }
         }
 
@@ -122,7 +118,7 @@ class Platinum28Degree
     {
         if (preg_match(
             '/retrieveJSON\((\{.*\}\}\})/i',
-            $this->cached_html,
+            $this->loadContent(),
             $matches
         )) {
             $json = json_decode($matches[1], true);
@@ -140,7 +136,7 @@ class Platinum28Degree
     {
         if (preg_match_all(
             '/div name=\"Transaction_([a-zA-Z]*)\">(.+)<\/div/',
-            $this->cached_html,
+            $this->loadContent(),
             $matches
         )) {
             array_walk(
@@ -170,12 +166,47 @@ class Platinum28Degree
      * Get the modified timestamp of cache file
      * @return int timestamp, or false on failure
      */
-    public function getCacheMTime()
+    public function isCacheValid()
     {
-        if (is_readable($this->cache_filepath)) {
-            return filemtime($this->cache_filepath);
+        if ($cache_filepath=$this->getCacheFilePath()) {
+            if (is_readable($cache_filepath)) {
+                $life_time = intval($this->config->get('cache.life-time'));
+                return time() - filemtime($cache_filepath) < $life_time;
+            }
         }
 
         return false;
+    }
+
+    /**
+     * return the location of cache file
+     * @return string the path, or blank on fail
+     */
+    protected function getCacheFilePath()
+    {
+        static $cache_directory = null;
+        if (is_null($cache_directory)) {
+            if ($this->config->get('cache.enabled')) {
+                if ($directory = $this->config->get('cache.directory')) {
+                    if ($directory[0]!='/') {
+                        // this is an relative path. Use the project dir as basedir
+                        $directory = __DIR__ . '/../../../' . $directory;
+                    }
+
+                    // directory exists?
+                    if (! file_exists($directory)) {
+                        mkdir($directory, 0770, true);
+                    }
+
+                    $cache_directory = $directory;
+                }
+            }
+        }
+
+        if ($cache_directory) {
+            return $cache_directory . '/codzo.p28d.cache';
+        }
+        
+        return '';
     }
 }
